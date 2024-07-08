b6143e4a69ea4c539e5634b4cebcdbefb464bbf1
Tobias Pflug <tobias.pflug@gmail.com>
2024-07-08 17:44:34 +0200

    create working nix scaffolding

--- nix/default.nix
+++ nix/default.nix
@@ -3,9 +3,29 @@
 let
   pins = import ../npins/default.nix;
   haskellNix = import pins."haskell.nix" { };
-  config = {
-    allowUnfree = true;
+  iohkNix = import pins."iohk-nix" { };
+  iohkNixCryptoOverlay = builtins.head iohkNix.overlays.crypto;
+  iohkNixCryptoInputs = {
+    sodium = pins.libsodium // { shortRev = "dbb48cce"; };
+    blst = pins.blst // { shortRev = "1l1c905"; };
+    secp256k1 = pins.secp256k1 // { shortRev = "acf5c55"; };
   };
-  overlays = [ ] ++ haskellNix.overlays;
+  overlays = [
+    (self: super: { CHaP = pins."cardano-haskell-packages"; })
+  ] ++ haskellNix.overlays ++ [
+    (self: super: { inherit iohkNix; })
+  ] ++ haskellNix.overlays
+  ++ [ (iohkNixCryptoOverlay iohkNixCryptoInputs) ]
+  ++ iohkNix.overlays."haskell-nix-extra"
+  ++ [
+    (self: super: {
+      haskell-nix = super.haskell-nix // {
+        extraPkgconfigMappings = super.haskell-nix.extraPkgconfigMappings or { } // {
+          "libblst" = [ "libblst" ];
+          "libsodium" = [ "libsodium-vrf" ];
+        };
+      };
+    })
+  ];
 in
-import pins.nixpkgs { inherit system overlays config; }
+import pins.nixpkgs { inherit system overlays; config = { allowUnfree = true; }; }
---
+++ onchain/default.nix
@@ -0,0 +1,24 @@
+{ system ? builtins.currentSystem
+, pkgs ? import ../nix { inherit system; }
+}:
+
+pkgs.haskell-nix.cabalProject' {
+  src = ./.;
+  compiler-nix-name = "ghc8107";
+  inputMap = {
+    "https://input-output-hk.github.io/cardano-haskell-packages" = pkgs.CHaP;
+  };
+  modules = [
+    {
+      packages = {
+        trustless-sidechain.doHaddock = false;
+        trustless-sidechain-prelude.doHaddock = false;
+        onchain-poc.doHaddock = false;
+        onchain-poc.flags.defer-plugin-errors = false;
+        trustless-sidechain.ghcOptions = [ "-Werror" ];
+        onchain-poc.ghcOptions = [ "-Werror" ];
+        trustless-sidechain-prelude.ghcOptions = [ "-Werror" ];
+      };
+    }
+  ];
+}
